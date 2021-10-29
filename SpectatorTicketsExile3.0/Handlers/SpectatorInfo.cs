55c02b8fcd656abfac9b97cfc325635f935f3f4d
undid-iridium <francis.x.irizarry@gmail.com>
2021-10-28 23:05:10 -0400

    Re-wrote logic based on assumption functions are SHARED across players. Meaning now it will update specific player hints. The solution for showing the hint in the first place is to put a very dumb.. long hint. For now, that is what I do.

--- SpectatorTicketsExile3.0/Handlers/SpectatorInfo.cs
+++ SpectatorTicketsExile3.0/Handlers/SpectatorInfo.cs
@@ -1,7 +1,6 @@
 ﻿using Exiled.API.Features;
 using Exiled.Events.EventArgs;
 using System;
-using System.Threading.Tasks;
 
 
 namespace SpectatorTickets_EXILED3.Handlers
@@ -10,48 +9,52 @@ namespace SpectatorTickets_EXILED3.Handlers
     class SpectatorInfo
     {
 
-        bool stop_showing_tickets = false;
-
         /// <summary>
         /// Purpose is to show hint in the bottom right corner on death regarding how many NTF/Chaos can spawn
+        /// I would use new Hint(); however, since EXILE took the ability to use custom hints, I can't
+        /// without updating their source code (Which I looked at). I don't know the limits of 
+        /// what I can do, and whether it breaks dependencies. I might fork and create what I need
+        /// and then have that be easier to use. 
         /// </summary>
         /// <param name="deathEvent"></param>
-        public async void OnDeath(DiedEventArgs deathEvent)
+        public void OnDeath(DiedEventArgs deathEvent)
         {
             //Automatically assume when death is called, if person died that they need to be shown hint.
-            stop_showing_tickets = false;
-            while (!stop_showing_tickets)
-            {
-                String message_to_use = new string('\n', 14) + $"<align=right><color=blue>NTF Tickets:</color> {Respawn.NtfTickets} </align>" +
+
+            String message_to_use = new string('\n', 14) + $"<align=right><color=blue>NTF Tickets:</color> {Respawn.NtfTickets} </align>" +
                         $"\n<align=right><color=green>Chaos Tickets:</color> {Respawn.ChaosTickets} </align>";
-                deathEvent.Target.ShowHint(message_to_use);
-                await Task.Delay(1000);
-            }
+            deathEvent.Target.ShowHint(message_to_use, 2000);
+
+
         }
 
 
         internal void OnRespawn(SpawningEventArgs respawnEvent)
         {
             respawnEvent.Player.Broadcast(1, "Welcome back to the living", Broadcast.BroadcastFlags.Normal, true);
-            this.stop_showing_tickets = true;
+            if (respawnEvent.Player.HasHint)
+            {
+                respawnEvent.Player.ShowHint("", 0);
+            }
+
         }
 
 
         /// <summary>
-        /// Function purpose is to stop showing him in OnDeath when round ends, respawn, etc. 
+        /// Function purpose is to stop showing hint when round ends, respawn, etc. 
         /// </summary>
         public void onRoundRestart()
         {
-            this.stop_showing_tickets = true;
+            Map.ShowHint("", 0);
         }
 
         /// <summary>
-        /// Function purpose is a dummy function to allow us to run this on round end to stop pushing hint in OnDeath
+        /// Function purpose is to force hint clear on RoundEnd
         /// </summary>
         /// <param name="args"></param>
         public void OnRoundEnd(EndingRoundEventArgs args)
         {
-            this.stop_showing_tickets = true;
+            Map.ShowHint("", 0);
         }
 
 
